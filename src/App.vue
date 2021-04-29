4ff6fedf68252f68278a384731c9575a5abf37f7
test <test@test.com>
2021-04-29 22:40:10 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <my-form :user="selectedUser"/>
+
+    <h1>Users</h1>
+    <button v-for="user in users" @click="selectedUser = user">{{user.firstname}}</button>
+  </div>
+</template>
+
+<script>
+import MyForm from '@/components/MyForm'
+
+export default {
+  name: 'App',
+
+  components: { MyForm },
+
+  data () {
+    return {
+      selectedUser: {
+        firstname: ''
+      },
+      users: [
+        { firstname: 'User1' },
+        { firstname: 'User2' },
+        { firstname: 'User3' }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <input v-model="form.firstname" type="text" placeholder="First Name" />
+    <input type="submit" />{{ user.firstname }}
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'MyForm',
+
+  props: [
+    'user'
+  ],
+
+  data() {
+    return {
+      form: {
+        firstname: this.user.firstname
+      }
+    }
+  },
+
+}
+</script>
